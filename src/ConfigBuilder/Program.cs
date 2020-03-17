b20ecec5cb907e7fa05b8973167a2b91e8286ba5
Ryan Condron <ryan.condron@jjventures.com>
2020-03-17 12:47:56 -0500

    implement config builder.

--- src/ConfigBuilder/Program.cs
+++ src/ConfigBuilder/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace ConfigBuilder
 {
@@ -6,7 +7,70 @@ namespace ConfigBuilder
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Outputting Entity Configuration");
+
+            FileStream fileStream = File.OpenRead(args[0]);
+            StreamReader reader = new StreamReader(fileStream);
+            int lineCount = 1;
+
+            while (!reader.EndOfStream)
+            {
+                // Skip Header Row
+                if (lineCount == 1)
+                {
+                    lineCount++;
+                    continue;
+                }
+
+                var line = reader.ReadLine();
+                var values = line.Split(',');
+
+                Console.WriteLine($@"builder.Property(prop => prop.{PropertyName(values[3])})
+                    .HasColumnName(""{values[3]}"")
+                    {ColumnLength(values)}
+                    .{IsRequired(values[10])};");
+
+                Console.WriteLine("");
+                Console.WriteLine("");
+            }
+        }
+
+        static string PropertyName(string columnName)
+        {
+            string[] parts = columnName.Split('_');
+            int i = 0;
+
+            foreach (var item in parts)
+            {
+                parts[i] = char.ToUpper(item[0]) + item.Substring(1);
+                i++;
+            }
+
+            return string.Join("", parts);
+        }
+
+        static string ColumnLength(string[] line)
+        {
+            string value;
+
+            if (line[5] == "decimal")
+            {
+                value = $@".HasColumnType(""decimal({line[6]},{line[8]})"")";
+            } else if (line[5] == "varchar" || line[5] == "char")
+            {
+                value = $@".HasMaxLength({line[7]})";
+            }
+            else
+            {
+                value = null;
+            }
+
+            return value;
+        }
+
+        static string IsRequired(string bit)
+        {
+            return bit == "0" ? "IsRequired()" : "IsRequired(false)";
         }
     }
 }
